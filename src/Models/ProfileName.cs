2147dbce348b29e25ff1d6d75ddcf1c9cd32aaea
Nikita Tulenev <n1tulenev@gmail.com>
2025-05-02 15:26:03 +0200

    Change ProfileName as record

--- src/Models/ProfileName.cs
+++ src/Models/ProfileName.cs
@@ -3,7 +3,7 @@ namespace Models;
 /// <summary>
 /// Represents the name of an environment profile.
 /// </summary>
-public sealed class ProfileName
+public sealed record class ProfileName
 {
     /// <summary>
     /// Gets the name of the profile.
@@ -14,7 +14,8 @@ public sealed class ProfileName
     /// Initializes a new instance of the <see cref="ProfileName"/> class with the specified profile name.
     /// </summary>
     /// <param name="name">The name of the profile. Cannot be null or whitespace.</param>
-    /// <exception cref="ArgumentException">Thrown when the <paramref name="name"/> is null, empty, or consists only of white-space characters.</exception>
+    /// <exception cref="ArgumentException">Thrown when the <paramref name="name"/>
+    /// is null, empty, or consists only of white-space characters.</exception>
     public ProfileName(string name)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
